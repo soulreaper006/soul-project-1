4cf50d1cd1b320847335323949a85dd20e3b5c5e
Soul <itsfahim233@gmail.com>
2024-01-10 00:04:54 +0530

    adding main code concept with some bugs

--- main.cpp
+++ main.cpp
@@ -1,8 +1,59 @@
-#include<iostream>
+#include <iostream>
+#include<vector>
 using namespace std;
+
+class product
+{
+private:
+public:
+    string product_name;
+    float quantity;
+    float price;
+
+    // functions
+    void input()
+    {
+        cout << "enter product name: ";
+        cin >> product_name;
+        cout << "enter product quantity: ";
+        cin >> quantity;
+        cout<<"enter product price: ";
+        cin >> price;
+        cout << "wann add next item ?" << endl
+             << "for 'YES' press 0 and for 'NO' perss 1 : "; 
+    }
+
+    void display()
+    {
+        cout << "product name: " << product_name << endl;
+        cout << "product name: " << quantity << endl;
+        cout << "product name: " << price << endl;
+    }
+};
 
 int main()
 {
-    cout<<"hello world";
+    int i=0;
+     product item[5];
+    bool next_item=1;
+    while ( i <= 3)
+    {
+       
+       
+        if (next_item == 0)
+        {
+            for(int j=0;j<=3;j++)
+            {
+            item[j].display();
+            }
+            break;
+        }
+        
+        item[i].input();
+        cin >> next_item;
+        i++;
+    }
+    
+
     return 0;
 }
